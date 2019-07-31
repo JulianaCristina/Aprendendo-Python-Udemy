2440866e11f8e3f8f4e3a1f1f79368e9b2487372
Juliana Gonçalves <juliana@pop-os.localdomain>
2019-07-31 16:07:07 -0300

    Random

--- hello.py
+++ hello.py
@@ -1,12 +1,10 @@
 #--coding: utf-8 --
 
-# dicionarios em python
-dicionario_sites = {"Diego": "diegomariano.com"}
-# Sera impresso "diegomariano.com
-print(dicionario_sites['Diego'])
+# números aleatórios
 
-# dicionarios em python
-dicionario_sites = {"Diego": "diegomariano.com", "Google": "google.com", "Udemy": "udemy.com"}
- 
-for chave in dicionario_sites:
-    print(dicionario_sites[chave])
+import random
+random.seed(1) #força ele a retornar sempre o msm número, no caso, a segunda posição
+#numero = random.randint(0,10)#escolhe um entre 0 e 10
+lista = [6,45,9]
+numero = random.choice(lista) #escolhe um número aleatório dentre os números da lista
+print(numero)
