cdbac6cd84496691474c650bfaa6481f598946ac
Juliana Gonçalves <juliana@pop-os.localdomain>
2019-07-31 09:43:44 -0300

    operadores logicos

--- hello.py
+++ hello.py
@@ -6,3 +6,18 @@ print("Olá Mundo")
 print(2+2) #soma
 print(2**3) #esponenciação
 print(10%3) #resto
+
+#variáveis
+mensagem="Olá mundo"
+print(mensagem)
+
+#Operadores relacionais
+x=3
+y=2
+print(x==y)
+
+#operadores lógicos
+soma = x+y
+print(x==y and x==soma)
+print(x==y or x==soma)
+
