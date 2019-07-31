c59c613e267a827a5c1f6ae9d1d8857d6884edac
Juliana Gonçalves <juliana@pop-os.localdomain>
2019-07-31 11:57:01 -0300

    Explorando Strings e suas funçẽos

--- hello.py
+++ hello.py
@@ -1,4 +1,37 @@
 #-*-coding: utf-8 -*-
-numero = input("Digite um numero: ")
-print("O número digitado é")
-print(numero)
+a = "Juliana"
+b = "Gonçalves"
+concatenar = a + " "+ b + "\n"
+print(concatenar)
+
+tamanho = len(concatenar)
+print(tamanho)
+
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
+print(a[6])
+
+print(concatenar[0:5]) #imprime de 0 à 4
+
+#imprime tudo em minusculo
+print(concatenar.lower())
+print(concatenar.upper())
+print(concatenar.strip()) #remove a linha depois da concatenação
+
+minha_string = "O rato roeu a roupa do rei de Roma"
+
+minha_lista = minha_string.split(" ")
+print(minha_lista)
+
+#Busca em substrings, pedaço de uma string
+busca = minha_string.find("rei")
+print(busca)
+print(minha_string[busca:]) #imprime a partir de rei, se não encontrar retorna -1
+
+minha_string.replace("rei", "rainha") #não funciona
+minha_string = minha_string.replace("o rei", "a rainha")
+print(minha_string)
