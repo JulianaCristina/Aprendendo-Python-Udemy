42f6f74cb3fe3f1b1a93126c4a987929eb118b45
Juliana Gonçalves <juliana@pop-os.localdomain>
2019-07-31 10:15:32 -0300

    laços de repetição

--- hello.py
+++ hello.py
@@ -41,9 +41,34 @@ a = 1
 b = 2
 
 
-if a == b:
-    print("a é igual a b")
-elif a < b:
-    print("a é menor que b")
-else:
-    print("y é maior que x")
+#if a == b:
+#    print("a é igual a b")
+#elif a < b:
+#    print("a é menor que b")
+#else:
+#    print("y é maior que x")
+
+
+#x = 1
+
+#while x < 10: 
+#    print(x)
+#    x = x+4
+
+
+lista1 = [1,2,3,4,5]
+lista2 = ["ola", "mundo","!"]
+lista3 = [0,"ola","biscoito", "bolacha",9.99, True]
+
+for i in lista3:
+    print(i)
+
+#função range
+for i in range(10):
+    print(i)
+
+for i in range(10,20): #vai imprimir de 10 à 19
+    print(i)
+
+for i in range(10,20,2): #vai imprimir de 10 à 18 só que de 2 em 2
+    print(i)
