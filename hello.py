eab0a2189a196170fb244a8e76e794112ca43aca
Juliana Gonçalves <juliana@pop-os.localdomain>
2019-07-31 13:54:16 -0300

    Arquivos- Abrindo e Lendo arquivos

--- hello.py
+++ hello.py
@@ -1,9 +1,24 @@
-def soma(x,y):
-    return(x+y)
-def multiplicacao(x,y):
-    return x*y
+ #--coding: utf-8 --
+# r = somente leitura
+# w =  escrita, cria um arquivo novo em branco, se existir apaga o existente
+# a = leitura e escrita (add o novo conteudo no final do arquivo)
+# r+(leitura e escrita), w+a(também apaga o conteudo anterior do arquivo), a+(abre o arquivo para atualização)
 
-s = soma(2,3)
-m = multiplicacao(2,5)
-#recursividade
-print(soma(s,m))
+#Lendo arquivos
+
+#read() - lê arquivo
+#readline() - lê uma linha
+#readlines() - lê o arquivo e o armazena em uma lista
+
+
+arquivo = open("arquivo.txt")
+#print(arquivo) #mostra o nome do arquivo que estou tentando abrir, o modo de leitura e o endereço na memória
+
+#linhas = arquivo.readlines() #converte em uma lista
+#print(linhas)
+
+#for linha in linhas:
+#    print(linha)
+
+texto_completo = arquivo.read()
+print(texto_completo)
