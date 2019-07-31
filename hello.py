596be60d72ddfbf7736cf1101b101b64da3cf4ce
Juliana Gonçalves <juliana@pop-os.localdomain>
2019-07-31 15:49:49 -0300

    Lista - Ordenaçao

--- hello.py
+++ hello.py
@@ -1,19 +1,10 @@
 #--coding: utf-8 --
-# r = somente leitura
-# w =  escrita, cria um arquivo novo em branco, se existir apaga o existente
-# a = leitura e escrita (add o novo conteudo no final do arquivo)
-# r+(leitura e escrita), w+a(também apaga o conteudo anterior do arquivo), a+(abre o arquivo para atualização)
 
-#Lendo arquivos
+lista = [50, 20, 10, 40, 30, 60]
 
-#read() - lê arquivo
-#readline() - lê uma linha
-#readlines() - lê o arquivo e o armazena em uma lista
+lista.sort() #esse metodo altera lista que já existe
 
-minha_lista = ["abacaxi", "melancia", "abacate"]
-minha_lista_2 = [1,2,3,4,5]
-
-#minha_lista.append("limão") #add
-#del minha_lista[2:] #deleta do 2 p frente
-#print(minha_lista)
-
+lista = sorted(lista) #cria uma nova lista
+lista.sort(reverse=True) #decrescente
+lista.reverse() #inverte a lista, torna o primeiro elemento o ultimo
+print(lista)
