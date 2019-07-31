237996d40306819f5af679026f52b4cd4f045e1a
Juliana Gonçalves <juliana@pop-os.localdomain>
2019-07-31 15:44:35 -0300

    Lista - Add e removendo

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
- #--coding: utf-8 --
+#--coding: utf-8 --
 # r = somente leitura
 # w =  escrita, cria um arquivo novo em branco, se existir apaga o existente
 # a = leitura e escrita (add o novo conteudo no final do arquivo)
@@ -10,19 +10,10 @@
 #readline() - lê uma linha
 #readlines() - lê o arquivo e o armazena em uma lista
 
+minha_lista = ["abacaxi", "melancia", "abacate"]
+minha_lista_2 = [1,2,3,4,5]
 
-arquivo = open("arquivo.txt")
-#print(arquivo) #mostra o nome do arquivo que estou tentando abrir, o modo de leitura e o endereço na memória
+#minha_lista.append("limão") #add
+#del minha_lista[2:] #deleta do 2 p frente
+#print(minha_lista)
 
-#linhas = arquivo.readlines() #converte em uma lista
-#print(linhas)
-
-#for linha in linhas:
-#    print(linha)
-
-texto_completo = arquivo.read()
-print(texto_completo)
-
-w=open("arquivo2.txt", "w")
-w.write("Esse é o meu arquivo")
-w.close
