1a5ae10a6d76bab78f10fd6c18914a08eebeecc0
Juliana Gonçalves <juliana@pop-os.localdomain>
2019-07-31 09:54:16 -0300

    condicionais

--- hello.py
+++ hello.py
@@ -1,23 +1,49 @@
 #-*-coding: utf-8 -*-
-print "Hello world"
-print("Outra mensagem")#comentario
-print("Olá Mundo")
+#print "Hello world"
+#print("Outra mensagem")#comentario
+#print("Olá Mundo")
 #operacoes matematicas
-print(2+2) #soma
-print(2**3) #esponenciação
-print(10%3) #resto
+#print(2+2) #soma
+#print(2**3) #esponenciação
+#print(10%3) #resto
 
 #variáveis
-mensagem="Olá mundo"
-print(mensagem)
+#mensagem="Olá mundo"
+#print(mensagem)
 
 #Operadores relacionais
-x=3
-y=2
-print(x==y)
+#x=3
+#y=2
+#print(x==y)
 
 #operadores lógicos
-soma = x+y
-print(x==y and x==soma)
-print(x==y or x==soma)
+#soma = x+y
+#print(x==y and x==soma)
+#print(x==y or x==soma)
 
+#Operadores Condicionais
+
+x = 1
+y = 10
+
+if x>y:
+    print("x é maior que y")
+if y>x:
+    print("y é maior que x")
+
+
+if x>y:
+    print("x é maior que y")
+else:
+    print("y é maior que x")
+
+a = 1
+b = 2
+
+
+if a == b:
+    print("a é igual a b")
+elif a < b:
+    print("a é menor que b")
+else:
+    print("y é maior que x")
